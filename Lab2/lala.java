ac742ee34408972e782c7fbde8d1644be7b228ef
LionVas <lionbird@mail.ru>
2025-12-14 22:32:32 +1000

    lala commit

---
+++ Lab2/lala.java
@@ -0,0 +1,19 @@
+int a = 3;
+int b = 1;
+int c = 5;
+int d = 7;
+int x;
+if (a > b) {
+    a += b
+}
+do {
+    a = a - 1;
+} while (a>b);
+
+for (int i = 1; i < 9; i++) {
+    x = 1;
+}
+
+while (c < d){
+    c += 1
+}
