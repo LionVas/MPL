0932b970da8c8accc7bf648ac77b74356c700294
LionVas <lionbird@mail.ru>
2025-12-14 22:32:59 +1000

    main commit

---
+++ Lab2/main.py
@@ -0,0 +1,14 @@
+from Lexer import Lexer
+
+#s = "int[] a = { 1, 2, 3, 5, 12, ыфв }"
+#s = "if (a < b) {  "
+strings = []
+with open("lala.java", encoding="utf-8") as file:
+    for line in file:
+        strings.append(Lexer.def_type(line))
+
+with open("translated.py", "w", encoding="utf-8") as file:
+    for s in strings:
+        #print(s)
+        file.write(s + "\n")
+print(Lexer.variables)
