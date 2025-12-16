5abbe657d579e251031840c81c60ca0449943ee0
Lion <165137090+LionVas@users.noreply.github.com>
2025-12-16 13:38:08 +1000

    Update main.py

--- Lab2/main.py
+++ Lab2/main.py
@@ -3,7 +3,7 @@ from Lexer import Lexer
 #s = "int[] a = { 1, 2, 3, 5, 12, ыфв }"
 #s = "if (a < b) {  "
 strings = []
-with open("lala.java", encoding="utf-8") as file:
+with open("sample.java", encoding="utf-8") as file:
     for line in file:
         strings.append(Lexer.def_type(line))
 
